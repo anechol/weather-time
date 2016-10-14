4ac1fad982b54d3d83d6c4c7d28f7a0f785cb285
anechol <ashleyne92@gmail.com>
2016-10-14 15:40:07 -0500

    Fixes quotes around strings and updates rain background image.

--- main/js/weather.js
+++ main/js/weather.js
@@ -1,76 +1,75 @@
 $(document).ready(function() {
-  // API Call
-  // NOTE: Don't allow API Key to show for the future
-  const url = `http://api.openweathermap.org/data/2.5/weather?id=4887398&appid={APIKEY}&units=imperial`;
+    // API Call
+    const url = `http://api.openweathermap.org/data/2.5/weather?id=4887398&appid={APIKEY}&units=imperial`;
 
-  // Weather elements
-  let city = document.getElementById('city');
-  let desc = document.getElementById('desc');
-  let temp = document.getElementById('temp');
-  let minTemp = document.getElementById('minTemp');
-  let maxTemp = document.getElementById('maxTemp');
-  let rain = document.getElementById('rain');
-  let snow = document.getElementById('snow');
+    // Weather elements
+    let city = document.getElementById('city');
+    let desc = document.getElementById('desc');
+    let temp = document.getElementById('temp');
+    let minTemp = document.getElementById('minTemp');
+    let maxTemp = document.getElementById('maxTemp');
+    let rain = document.getElementById('rain');
+    let snow = document.getElementById('snow');
 
-  // Arrays for weather conditions
-  const rainWords = ['Rain', 'Shower', 'Showers', 'Drizzle'];
-  const cloudWords = ['Cloud', 'Clouds', 'Cloudy'];
-  const clearWords = ['Clear', 'Sun', 'Sunny'];
+    // Arrays for weather conditions
+    const rainWords = ['Rain', 'Shower', 'Showers', 'Drizzle'];
+    const cloudWords = ['Cloud', 'Clouds', 'Cloudy'];
+    const clearWords = ['Clear', 'Sun', 'Sunny'];
 
-  // Get JSON data from API Call
-  $.getJSON(url, function(data) {
-    city.innerHTML = 'Chicago'; // City Name
-    desc.innerHTML = `${JSON.stringify(data.weather[0].main)}: ${JSON.stringify(data.weather[0].description)}`; // Description of weather
-    temp.innerHTML = `Temp: ${Math.round(JSON.stringify(data.main.temp))}${'&deg;'}`; // Current Temperature
-    minTemp.innerHTML = `Low: ${Math.round(JSON.stringify(data.main.temp_min))}${'&deg;'}`; // Min Temperature
-    maxTemp.innerHTML = `Hi: ${Math.round(JSON.stringify(data.main.temp_max))}${'&deg;'}`; // Max Temperature
+    // Get JSON data from API Call
+    $.getJSON(url, function(data) {
+        let updateWeather = () => {
 
-    // Rain - If none, don't show
-    if (JSON.stringify(data.rain['1h']) == undefined) {
-      rain.style.display = 'none';
-    } else {
-      rain.innerHTML = `Rain: ${JSON.stringify(data.rain['1h'])} in`;
-    }
+            city.innerHTML = 'Chicago'; // City Name
+            desc.innerHTML = `${JSON.parse(JSON.stringify(data.weather[0].main))}: ${JSON.parse(JSON.stringify(data.weather[0].description))}`; // Description of weather
+            temp.innerHTML = `Temp: ${Math.round(JSON.parse(JSON.stringify(data.main.temp)))}${ '&deg;'}`; // Current Temperature
+            minTemp.innerHTML = `Low: ${Math.round(JSON.parse(JSON.stringify(data.main.temp_min)))}${ '&deg;'}`; // Min Temperature
+            maxTemp.innerHTML = `Hi: ${Math.round(JSON.parse(JSON.stringify(data.main.temp_max)))}${ '&deg;'}`; // Max Temperature
 
-    // Snow - If none, don't show
-    // FIXME: This needs to be fixed once it starts to snow. The background will not work for some reason when using ['1h'].
-    if (JSON.stringify(data.snow) == undefined) {
-      snow.style.display = 'none';
-    } else {
-      snow.innerHTML = `Snow: ${JSON.stringify(data.snow['1h'])} in`;
-    }
+            // Rain - If not raining in the last hour, don't show
+            if (JSON.stringify(data.rain) == undefined || JSON.stringify(data.rain['1h']) == undefined) {
+                rain.style.display = 'none';
+            } else {
+                rain.innerHTML = `Rain: ${JSON.parse(JSON.stringify(data.rain['1h']))} in`;
+            }
 
-    // Change background based on weather
-    // Loops through a list of words to find a match. When match is made,
-    // appropriate background is used
+            // Snow - If none, don't show
+            // FIXME: This needs to be fixed once it starts to snow. The background will not work for some reason when using ['1h'].
+            if (JSON.stringify(data.snow) == undefined || JSON.stringify(data.snow['1h'])) {
+                snow.style.display = 'none';
+            } else {
+                snow.innerHTML = `Snow: ${JSON.parse(JSON.stringify(data.snow['1h']))} in`;
+            }
 
-    // Rain
-    for (let word = 0; word < rainWords.length; word++) {
-      if (JSON.stringify(data.weather[0].main).includes(rainWords[word])) {
-        document.body.style.background = 'url("main/img/rain.jpg") no-repeat center';
-      }
-    }
+            // Change background based on weather
+            // Loops through a list of words to find a match. When match is made,
+            // appropriate background is used
 
-    // Clouds
-    for (let word = 0; word < cloudWords.length; word++) {
-      if (JSON.stringify(data.weather[0].main).includes(cloudWords[word])) {
-        document.body.style.background = 'url("main/img/cloudy.jpg") no-repeat top left';
-      }
-    }
+            // Rain
+            for (let word = 0; word < rainWords.length; word++) {
+                if (JSON.stringify(data.weather[0].main).includes(rainWords[word])) {
+                    document.body.style.background = 'url("main/img/rain.jpg") no-repeat center';
+                }
+            }
 
-    // Clear skies
-    for (let word = 0; word < clearWords.length; word++) {
-      if (JSON.stringify(data.weather[0].main).includes(clearWords[word])) {
-        document.body.style.background = 'url("main/img/clearsky.jpg") no-repeat top center';
-      }
-    }
+            // Clouds
+            for (let word = 0; word < cloudWords.length; word++) {
+                if (JSON.stringify(data.weather[0].main).includes(cloudWords[word])) {
+                    document.body.style.background = 'url("main/img/cloudy.jpg") no-repeat left center';
+                }
+            }
 
-    // TODO: Try to make this update the temp and description whenever it updates.
-    // This checks every second.
-    let weatherUpdate = () => {
-      return [desc, temp, minTemp, maxTemp];
-    };
+            // Clear skies
+            for (let word = 0; word < clearWords.length; word++) {
+                if (JSON.stringify(data.weather[0].main).includes(clearWords[word])) {
+                    document.body.style.background = 'url("main/img/clearsky.jpg") no-repeat top center';
+                }
+            }
+        }
 
-    setInterval(weatherUpdate, 1000);
-  });
+        // TODO: Try to make this update the temp and description whenever it updates.
+        // This checks every second.
+
+        setInterval(updateWeather, 1000);
+    });
 });
